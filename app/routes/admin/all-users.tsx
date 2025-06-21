785a7c01f4f5c9ed4d1a6e80ef79bd15fb9a4e3c
Arunjith <arunjithk987@Gmail.com>
2025-06-21 14:35:26 +0530

    Implemented Routing,Dashbaord Layout & UI, Auth Functional

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Header} from "../../../components";
+
+const AllUsers = () => {
+    return (
+        <main className="dashboard wrapper">
+            <Header
+                title={`Trips page`}
+                description="check out our current users in real time"
+            />
+
+            all Users page Contents
+        </main>
+    )
+}
+export default AllUsers
